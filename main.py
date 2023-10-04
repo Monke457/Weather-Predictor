7791702ad2684f13adaec50a7e06271ccd893f75
Beth Williams <beth.williams@bluewin.ch>
2023-10-04 16:12:20 +0200

    refactor: refined the model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 
@@ -11,7 +13,8 @@ pd.set_option("display.max_columns", None)
 weather = pd.read_csv("london_weather.csv", index_col="date")
 
 # Select relevant columns.
-core_weather = weather[["max_temp", "min_temp", "precipitation", "snow_depth"]].copy()
+core_weather = weather[["max_temp", "min_temp", "precipitation", "snow_depth",
+                        "mean_temp", "pressure", "cloud_cover", "sunshine"]].copy()
 
 # Convert the date to datetime.
 core_weather.index = pd.to_datetime(core_weather.index, format="%Y%m%d")
@@ -21,62 +24,104 @@ core_weather["snow_depth"] = core_weather["snow_depth"].fillna(0)
 core_weather["precipitation"] = core_weather["precipitation"].fillna(0)
 core_weather["max_temp"] = core_weather["max_temp"].ffill()
 core_weather["min_temp"] = core_weather["min_temp"].ffill()
+core_weather["cloud_cover"] = core_weather["cloud_cover"].fillna(7)
+core_weather["pressure"] = core_weather["pressure"].fillna(101790.0)
+
+# Create mean temps to fill null
+mean_temps = (core_weather["max_temp"] + core_weather["min_temp"]) / 2
+core_weather["mean_temp"] = core_weather["mean_temp"].fillna(mean_temps)
+
+
+# Add rolling means
+core_weather["rolling_max"] = core_weather["max_temp"].rolling(30).mean()
+core_weather = core_weather.iloc[30:, :].copy()
+
+# Add difference between monthly average and daily temperature
+core_weather["rolling_max_diff"] = core_weather["rolling_max"] - core_weather["max_temp"]
+
+# Add difference between max and min temperatures
+core_weather["max_min_diff"] = core_weather["max_temp"] - core_weather["min_temp"]
+
+# Add monthly average
+core_weather["monthly_avg"] = core_weather["max_temp"].groupby(core_weather.index.month)\
+    .transform(lambda x: x.expanding(1).mean())
+
+# Add day of year average
+core_weather["day_of_year_avg"] = core_weather["max_temp"].groupby(core_weather.index.day_of_year)\
+    .transform(lambda x: x.expanding(1).mean())
 
 # Set the target values.
 core_weather["target"] = core_weather.shift(-1)["max_temp"]
 core_weather = core_weather.iloc[:-1, :].copy()
 
+# Set the predictors (features).
+predictors = ["max_temp", "min_temp", "precipitation", "snow_depth", "rolling_max",
+              "rolling_max_diff", "max_min_diff", "monthly_avg", "day_of_year_avg",
+              "mean_temp", "pressure", "cloud_cover", "sunshine"]
 
-def create_predictions(predictors, data, reg):
+# Initialize a ridge model.
+reg = Ridge(alpha=.1)
+
+# Initialize a decision tree model.
+reg1 = DecisionTreeRegressor(random_state=0)
+
+# Initialize a random forest model
+reg2 = RandomForestRegressor(max_depth=90, random_state=0, n_estimators=100)
+
+
+def create_predictions(features, data, model):
     """
     A function for predicting the daily maximum temperature using a regression model.
-    :param predictors: an array of strings describing the columns of the data to use as predictors.
+    :param features: an array of strings describing the columns of the data to use as predictors.
     :param data: a DataFrame object containing all the weather data.
-    :param reg: the Ridge regression model to use to generate the predictions.
-    :return error: the average difference between the target values and predictions.
-            combined: a DataFrame object containing the target values and predictions.
+    :param model: the Ridge regression model for generating the predictions.
+    :return err: the average difference between the target values and predictions.
+            result: DataFrame object containing the target values and predictions.
     """
-
-    # Set the predictors (features).
-    predictors = ["max_temp", "min_temp", "precipitation", "snow_depth"]
 
     # Split the data.
     train = data.loc[:"2018-12-31"]
     test = data.loc["2019-01-01":]
 
-    # Initialize a regression model.
-    reg = Ridge(alpha=.1)
-
     # Fit the data to the model.
-    reg.fit(train[predictors], train["target"])
+    model.fit(train[features], train["target"])
 
     # Make predictions.
-    predictions = reg.predict(test[predictors])
+    predictions = model.predict(test[predictors])
 
     # Get the average prediction accuracy.
-    error = mean_absolute_error(test["target"], predictions)
+    err = mean_absolute_error(test["target"], predictions)
 
     # Combine the target and prediction values.
-    combined = pd.concat([test["target"], pd.Series(predictions, index=test.index)], axis=1)
-    combined.columns = ["actual", "predictions"]
+    result = pd.concat([test["target"], pd.Series(predictions, index=test.index)], axis=1)
+    result.columns = ["actual", "prediction"]
 
-    return error, combined
+    return err, result
 
 
-core_weather["month_max"] = core_weather["max_temp"].rolling(30).mean()
-core_weather["month_day_max"] = core_weather["month_max"] / core_weather["max_temp"]
-print(core_weather)
+error, combined = create_predictions(predictors, core_weather, reg)
+print(error)
+print(combined)
 
-""" 
+combined["diff"] = (combined["actual"] - combined["prediction"]).abs()
+print(combined.sort_values("diff", ascending=False))
+
+combined.plot()
+plt.show()
+
+"""
 ## Prediction analysis.
+# Show the weights for each predictor.
+print(reg.coef_)
+
+# Show predictor correlation
+print(core_weather.corr()["target"])
 
 # Plot the predictions.
 combined.plot()
 plt.show()
-
-# Show the weights for each predictor.
-print(reg.coef_)
 """
 
 # print(core_weather.apply(pd.isnull).sum())
 # print(core_weather.groupby(core_weather.index.year).sum())
+
